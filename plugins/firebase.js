f56a228051996499189561ef7f8d9ca144e9bd4c
ydyd1115 <ydyd1115@gmail.com>
2022-09-29 22:01:49 +0900

    first-commit

---
+++ plugins/firebase.js
@@ -0,0 +1,15 @@
+if (!firebase.apps.length) {
+  firebase.initializeApp(
+    {
+    apiKey: "AIzaSyCXnZWOzKrQ_RKPI8IqQOa5zW7YaR7IiQA",
+    authDomain: "fir-pj-a04be.firebaseapp.com",
+    projectId: "fir-pj-a04be",
+    storageBucket: "fir-pj-a04be.appspot.com",
+    messagingSenderId: "932649521994",
+    appId: "1:932649521994:web:c337151ce2bc71282367b3",
+    measurementId: "G-0DQWE2B6F5"
+    }
+  )
+}
+  
+export default firebase
